273aef0b5c6b29b6fd16f9ef4c4b545b02c76faa
kervyrivas <k.rivas@pucp.pe>
2018-03-01 21:43:10 -0500

    freeze embedding layer

--- Model.py
+++ Model.py
@@ -10,7 +10,7 @@ from Preprocessing import SOS_token
 ######################### ENCODER ###########################
 
 class Encoder_rnn(nn.Module):
-    def __init__(self, input_size, hidden_size, emb_size=None, n_layers=2, dropout=0.1, lang=None, use_optim_emb=True, USE_CUDA=False):
+    def __init__(self, input_size, hidden_size, emb_size=None, n_layers=2, dropout=0.1, lang=None, use_optim_emb=False, USE_CUDA=False):
         super(Encoder_rnn, self).__init__()
         
         self.input_size = input_size
@@ -24,10 +24,9 @@ class Encoder_rnn(nn.Module):
         
         self.embedding = nn.Embedding(input_size, emb_size)
         if lang:
-            self.embedding.weight.data.copy_(lang.vocab.vectors)
-            
-        if not use_optim_emb:
-            self.embedding.weight.required_grad = False
+            self.embedding.weight.data.copy_(lang.vocab.vectors)           
+            if not use_optim_emb:
+                self.embedding.weight.required_grad = False
             
         self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=True, batch_first=False)
         
@@ -101,7 +100,7 @@ class Global_attn(nn.Module):
 ######################### DECODER ###########################
 
 class Attn_decoder_rnn(nn.Module):
-    def __init__(self, attn_model, hidden_size, output_size, emb_size=None, n_layers=1, dropout=0.1, lang=None, use_optim_emb=False, USE_CUDA=False):
+    def __init__(self, attn_model, hidden_size, output_size, emb_size=None, n_layers=1, dropout=0.1, lang=None, use_optim_emb=True, USE_CUDA=False):
         super(Attn_decoder_rnn, self).__init__()
 
         # Keep for reference
@@ -121,9 +120,8 @@ class Attn_decoder_rnn(nn.Module):
         self.embedding = nn.Embedding(output_size, self.emb_size)
         if(lang):
             self.embedding.weight.data.copy_(lang.vocab.vectors)
-            
-        if not use_optim_emb:
-            self.embedding.weight.required_grad = False
+            if not use_optim_emb:
+                self.embedding.weight.required_grad = False
 
         self.embedding_dropout = nn.Dropout(dropout)
         self.lstm = nn.LSTM(self.emb_size, hidden_size, n_layers, dropout=dropout)
